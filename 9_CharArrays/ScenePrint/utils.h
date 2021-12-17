175ada9f986dec95746e12bb92f85dbc97d3f6c3
franneck94 <schaffranek.jan@web.de>
2021-12-17 10:47:55 +0100

    added

--- 9_CharArrays/ScenePrint/utils.h
+++ 9_CharArrays/ScenePrint/utils.h
@@ -1,98 +1,22 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
-
-#include <math.h>
 #include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-void print_int8_array(const int8_t *const array, const size_t length)
-{
-    for (size_t i = 0; i < length - 1; i++)
-    {
-        printf("%d, ", array[i]);
-    }
+void print_int8_array(const int8_t *const array, const size_t length);
 
-    printf("%d\n", array[length - 1]);
-}
+void print_int32_array(const int32_t *const array, const size_t length);
 
-void print_int32_array(const int32_t *const array, const size_t length)
-{
-    for (size_t i = 0; i < length - 1; i++)
-    {
-        printf("%d, ", array[i]);
-    }
+void print_uint8_array(const uint8_t *const array, const size_t length);
 
-    printf("%d\n", array[length - 1]);
-}
+void print_uint32_array(const uint32_t *const array, const size_t length);
 
-void print_uint8_array(const uint8_t *const array, const size_t length)
-{
-    for (size_t i = 0; i < length - 1; i++)
-    {
-        printf("%u, ", array[i]);
-    }
+void print_float_array(const float *const array, const size_t length);
 
-    printf("%u\n", array[length - 1]);
-}
+void print_double_array(const double *const array, const size_t length);
 
-void print_uint32_array(const uint32_t *const array, const size_t length)
-{
-    for (size_t i = 0; i < length - 1; i++)
-    {
-        printf("%u, ", array[i]);
-    }
+void clear_console();
 
-    printf("%u\n", array[length - 1]);
-}
-
-void print_float_array(const float *const array, const size_t length)
-{
-    for (size_t i = 0; i < length - 1; i++)
-    {
-        printf("%f, ", array[i]);
-    }
-
-    printf("%f\n", array[length - 1]);
-}
-
-void print_double_array(const double *const array, const size_t length)
-{
-    for (size_t i = 0; i < length - 1; i++)
-    {
-        printf("%lf, ", array[i]);
-    }
-
-    printf("%lf\n", array[length - 1]);
-}
-
-void clear_console()
-{
-#if defined _WIN32
-    system("cls");
-#elif defined(__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
-    system("clear");
-#elif defined(__APPLE__)
-    system("clear");
-#else
-    system("clear");
-#endif
-}
-
-
-void sleep_console(int ms)
-{
-#ifdef _WIN32
-    Sleep(ms);
-#else
-    usleep(ms * 1000);
-#endif
-}
+void sleep_console(int ms);
 
 #endif /* UTILS_H */
