51f9433e10136bc67c69a699b6c19ceff56a98f0
Kautzi <alexanderkautz.ak@gmail.com>
2023-04-20 06:07:36 +0200

    NEXT 7

--- 02_Variables/MathComputations/Exercise/main.c
+++ 02_Variables/MathComputations/Exercise/main.c
@@ -10,12 +10,15 @@ float power(float num, uint32_t power); //the power func takes a float var and r
 //### MAIN ###
 int main()
 {
-float res=0.0f;
+
 float x = -2.0F;
 float y = -3.0F;
 
 
-printf("%0.5f\n",power(2,5));
+printf("z1=%0.5f\n",(power(x,2)+power(y,2)-(x*y)+2));
+printf("z2=%0.2f\n",power((x-y),3)-3);
+printf("z3=%0.2f\n",((2.0f*(power(x,3)))-(0.5*(power(x,2)))-x+4)/y);
+
 
     return 0;
 }
@@ -26,7 +29,7 @@ printf("%0.5f\n",power(2,5));
 float power(float num, uint32_t power)
 {
     float result = num;//F has to be inserted because the compiler thinks as standart for floating point nums as double
-    for(uint32_t i = 0; i < power;i++ )
+    for(uint32_t i = 1; i < power;i++ )
     {
         result *= num;
     }
