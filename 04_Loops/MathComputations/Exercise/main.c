660525f89e23fdcae84d527319756ea4e8a35e18
Kautzi <alexanderkautz.ak@gmail.com>
2023-04-25 05:57:14 +0200

    NEXT 10

--- 04_Loops/MathComputations/Exercise/main.c
+++ 04_Loops/MathComputations/Exercise/main.c
@@ -8,12 +8,18 @@ int main()
     uint32_t num_iterations = 1000000;
     double pi_calculated = 0.0;
 
+    for(uint32_t i = 0; i < num_iterations; i++)
+    {
+        pi_calculated += (1.0/(4.0*i+1)) - (1.0/(4.0*i+3));
+    }
+
     double pi = 3.14159265358979323846;
-    printf("pi (calculated): %lf\n", pi_calculated);
+    printf("pi (calculated): %lf\n", pi_calculated*4);
     printf("pi (correct): %lf\n", pi);
 
     // print_dec_to_bin
     uint8_t dec = 142;
+
 
     return 0;
 }
