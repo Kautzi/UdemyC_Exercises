efbe15b62601ab96a2ae8def42b453a745136fe0
Kautzi <alexanderkautz.ak@gmail.com>
2023-04-28 06:10:30 +0200

    NEXT 13

--- 05_Functions/CharConverter/Exercise/lib.c
+++ 05_Functions/CharConverter/Exercise/lib.c
@@ -4,55 +4,89 @@
 
 bool is_numeric(const char character)
 {
-     if(character >= 48 && character <= 57)
+     if(character >= '0' && character <= '9')
     {
     return true;
     }
+    else
+    {
     return false;
+    }
 }
 
 bool is_alpha(const char character)
 {
-    if(is_alpha_numeric(character) == 1 && is_numeric(character) == 0)
-    {
+
+
         if((character >= 'A' && character <= 'Z')||(character >= 'a' && character <= 'z' ))
         {
         return true;
         }
+        else
+        {
         return false;
-    }
-    return false;
+        }
+
+
 }
 
 bool is_alpha_numeric(const char character)
 {
-    if(character <= 47 || (character >=58 && character <= 64)||(character >= 91 && character <= 96 ))//||(character >= 123 && character <= 127))
+    if(is_alpha(character)||is_numeric(character))
+    {
+    return true;
+    }
+    else
     {
     return false;
     }
-    return true;
 }
 
 bool is_upper_case(const char character)
 {
-    if(is_alpha(character) == 1)
-    {
-        if((character >= 65 && character <= 90))
+
+
+        if((character >= 'A') && (character <= 'Z'))
         {
             return true;//is upper
         }
+        else
+        {
         return false;//is lower
-    }
-    return false;
+        }
+
 }
 
 bool is_lower_case(const char character)
 {
 
-    return is_upper_case(character);
+    if ((character >= 'a') && (character <= 'z'))
+    {
+        return true;
+    }
+    else
+    {
+        return false ;
+    }
+
 }
 
+
 char to_upper_case( char character)
+{
+    if(is_lower_case(character)==true)
+    {
+        character -= 32;
+    }
+    else
+    {
+       return character;
+
+    }
+
+}
+
+char to_lower_case( char character)
 {
     if(is_upper_case(character)==true)
     {
@@ -60,13 +94,7 @@ char to_upper_case( char character)
     }
     else
     {
-        character -= 32;
+       return character;
 
     }
-    return character;
-}
-
-char to_lower_case(const char character)
-{
-    return to_upper_case(  character);
 }
