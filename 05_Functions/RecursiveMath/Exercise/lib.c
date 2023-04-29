534919edf7947f91d4262d0c59c6f8a6bd887b2a
Kautzi <alexanderkautz.ak@gmail.com>
2023-04-29 07:44:29 +0200

    NEXT 14

--- 05_Functions/RecursiveMath/Exercise/lib.c
+++ 05_Functions/RecursiveMath/Exercise/lib.c
@@ -4,8 +4,26 @@
 
 uint32_t recursive_sum(uint32_t lower_value, uint32_t upper_value)
 {
+    if(lower_value <= upper_value)
+    {
+        return lower_value + recursive_sum(lower_value + 1, upper_value);
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 uint32_t recursive_product(uint32_t lower_value, uint32_t upper_value)
 {
+
+    if(lower_value <= upper_value)
+    {
+        return lower_value * recursive_product(lower_value + 1, upper_value);
+    }
+
+    else
+    {
+    return 1;
+    }
 }
