639f4b02c8dfadb1fb5c108fd18787b8121f76fd
Kautzi <alexanderkautz.ak@gmail.com>
2023-05-26 06:05:22 +0200

    NEXT 28

---
+++ 10_Structs/LateralController/Exercise/AdFunctions.h
@@ -0,0 +1,49 @@
+
+#ifndef AD_FUNCTIONS_H
+#define AD_FUNCTIONS_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+#include "AdTypes.h"
+
+void print_scene(const VehicleType * ego_vehicle, const NeighborVehiclesType * vehicle);
+
+float kph_to_mps(const float kph);
+
+void init_ego_vehicle(VehicleType *ego_vehicle);
+
+void init_vehicles(NeighborVehiclesType *vehicles);
+
+void init_vehicle(VehicleType * vehicle,const int32_t id,const float speed_mps,const float distance,const LaneAssociationType lane);
+
+void print_vehicle(const VehicleType *vehicle);
+
+void print_neighbor_vehicles(const NeighborVehiclesType *vehicles);
+
+void compute_future_distance(VehicleType * vehicle,
+                             const float ego_driven_distance,
+                             const float seconds);
+
+void compute_future_state(const VehicleType * ego_vehicle,
+                          NeighborVehiclesType * vehicles,
+                          const float seconds);
+
+float mps_to_kph(const float mps);
+
+void decrease_speed(VehicleType *ego_vehicle);
+
+void longitudinal_control(const VehicleType *front_vehicle, VehicleType *ego_vehicle);
+
+const VehicleType *get_vehicle_array(const LaneAssociationType ego_lane,
+                                     const NeighborVehiclesType *vehicles);
+
+LaneAssociationType get_lane_change_request(const VehicleType *ego_vehicle,
+                                            const NeighborVehiclesType  *vehicles);
+
+bool lateral_control(const NeighborVehiclesType *vehicles,
+                     const LaneAssociationType lane_change_request,
+                     VehicleType *ego_vehicle);
+
+#endif
